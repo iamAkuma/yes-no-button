597266b79abc3e3d7d0b26821ac0473bf9ed957f
Saral Bajracharya <rmanbajra@gmail.com>
2024-02-11 19:31:29 +0545

    No moves to a random position when hovered

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react';
 
 const getRandomPosition = () => ({
-  top: `${Math.random() * 70}vh`,
-  left: `${Math.random() * 70}vw`,
+  top: `${Math.random() * window.innerHeight}px`,
+  left: `${Math.random() * window.innerWidth}px`,
 });
+
+const distanceThreshold = 150; // Threshold to ensure the button moves away from the mouse
 
 const App = () => {
   const [noButtonPosition, setNoButtonPosition] = useState(getRandomPosition());
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  const handleNoButtonClick = () => {
-    setNoButtonPosition(getRandomPosition());
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleNoButtonHover = () => {
+    let newPosition = getRandomPosition();
+    while (
+      Math.sqrt(
+        Math.pow(newPosition.left - mousePosition.x, 2) + Math.pow(newPosition.top - mousePosition.y, 2)
+      ) < distanceThreshold
+    ) {
+      newPosition = getRandomPosition();
+    }
+    setNoButtonPosition(newPosition);
   };
 
   return (
-    <div className="container"> {/* Use className instead of style */}
-      <div className="box"> {/* Use className instead of style */}
-        <button className="button">Yes</button> {/* Use className instead of style */}
-        <button className="button" style={noButtonPosition} onClick={handleNoButtonClick}>
+    <div className="container" onMouseMove={handleMouseMove}>
+      <div className="box">
+        <button className="button">Yes</button>
+        <button className="button" style={noButtonPosition} onMouseEnter={handleNoButtonHover}>
           No
         </button>
       </div>
