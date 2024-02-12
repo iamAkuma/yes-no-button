5ef9769b763b8c75f64b34ae0e9e7a782ed5c482
Saral Bajracharya <rmanbajra@gmail.com>
2024-02-12 19:17:39 +0545

    UI Changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// App.js
+
 import React, { useState, useEffect } from 'react';
 import './styles.css'; // Import the CSS file
 
@@ -8,6 +10,8 @@ const getRandomPosition = () => ({
 
 const App = () => {
   const [noButtonPosition, setNoButtonPosition] = useState({}); // Initialize position as empty object
+  const [message, setMessage] = useState(""); // State to store message
+  const [showMessage, setShowMessage] = useState(false); // State to toggle message display
 
   // Function to center the button on page load
   useEffect(() => {
@@ -31,17 +35,33 @@ const App = () => {
     setNoButtonPosition(newPosition); // Move button to the new random position
   };
 
+  // Function to handle click event on the "Yes" button
+  const handleYesButtonClick = () => {
+    setMessage("I knew it, bill's on you");
+    setShowMessage(true); // Show message
+  };
+
   return (
     <div className="container">
-      <div className="box">
-        <button className="button">Yes</button>
-        <button
-          className="button"
-          style={noButtonPosition}
-          onMouseEnter={handleNoButtonHover}
-        >
-          No
-        </button>
+      <div className={`box ${showMessage ? 'message-displayed' : ''}`}>
+        <div>
+          <h1>Wanna go out with me?</h1>
+        </div>
+        <div className="buttons-container">
+          {!showMessage && (
+            <>
+              <button className="button" onClick={handleYesButtonClick}>Yes</button>
+              <button
+                className="button"
+                style={noButtonPosition}
+                onMouseEnter={handleNoButtonHover}
+              >
+                No
+              </button>
+            </>
+          )}
+        </div>
+        {showMessage && <p>{message}</p>} {/* Display message if it exists */}
       </div>
     </div>
   );
